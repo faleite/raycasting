4de6f9abb1ca753a7ff6e84a3e90e618c59daa67
Fabricio Leite <fabricio_2310@hotmail.com>
2024-04-26 22:10:23 +0100

    Made the basic tutorial

--- README.md
+++ README.md
@@ -5,3 +5,27 @@ Este repositório acompanha o tutorial de raycasting disponível na página Wiki
 O objetivo deste tutorial é fornecer uma introdução prática e detalhada ao conceito de raycasting,\
 uma técnica fundamental em computação gráfica e jogos.
 
+## Notas
+- Para cada raio lançado temos que calcular a direção do mesmo para iterar as posições deste raio e encontrar alguma parede. A posição inicial do raio começa na posição do jogador e segue adiante em relação ao FOV (campo de visão) do jogador.
+
+![](https://github.com/vinibiavatti1/RayCastingTutorial/raw/master/resources/Raycasting%20projection.png?raw%3Dtrue)
+
+### RayCasting
+- Para iniciar nossa lógica, precisamos conhecer alguns conceitos com: **player angle, player FOV e Screen width**.\
+ - A primeira coisa que precisamos saber é que cada raio precisa ser lançado em relação ao ângulo do jogador e ao Campo\
+de Visão (FOV). O FOV do jogador é 60º, mas o foco do jogador está no meio do FOV. Por isso temos que iniciar o\
+RayCasting no -30º ângulo do jogador. Por exemplo, se o ângulo do jogador for 90º, temos que lançar os raios de 60º\
+um ângulo para 120º outro. Confira a imagem abaixo para ver a representação do FOV do jogador.
+
+![alt text](https://github.com/vinibiavatti1/RayCastingTutorial/raw/master/resources/FOV2.png?raw%3Dtrue)
+- **As primeiras coordenadas do raio são as mesmas das coordenadas do jogador. Criaremos um objeto com esses valores\
+para ficar mais organizado.**
+
+- A altura da parede será usada para desenhar as listras. Esta etapa é dividida em três operações.
+ - Desenhe o teto (do topo da tela até a meia altura da tela menos a altura da parede)
+ - Desenhe a parede (da meia altura da tela menos a altura da parede até a meia altura da tela mais a altura da parede)
+ - Desenhe o chão (da meia altura da tela mais a altura da parede até a parte inferior da tela)
+
+Recomendo seguir esta imagem para criar a lógica:
+
+![alt text](https://github.com/vinibiavatti1/RayCastingTutorial/raw/master/resources/stripes.png?raw%3Dtrue)
---
+++ raycasting.js
@@ -0,0 +1,275 @@
+// Data (Struct of values)
+let data = {
+	screen: {
+		width: 640,
+		height: 480,
+		halfWidth: null,
+		halfHeight: null
+	},
+	render: {
+		delay: 30
+	},
+	rayCasting: {
+		incrementAngle: null,
+		precision: 64
+	},
+	player: {
+		fov: 60,
+		halfov: null,
+		x: 2,
+		y: 2,
+		angle: 90,
+		// moves
+		speed: {
+			moviment: 0.5,
+			rotation: 5.0
+			}
+		},
+	map: [
+		[1,1,1,1,1,1,1,1,1,1],
+		[1,0,0,0,0,0,0,0,0,1],
+		[1,0,0,1,1,0,1,0,0,1],
+		[1,0,0,1,0,0,1,0,0,1],
+		[1,0,0,1,0,0,1,0,0,1],
+		[1,0,0,1,0,0,1,0,0,1],
+		[1,0,0,1,1,0,1,0,0,1],
+		[1,0,0,0,0,0,0,0,0,1],
+		[1,0,0,0,0,0,0,0,0,1],
+		[1,1,1,1,1,1,1,1,1,1],
+	],
+	key: {
+		up: "KeyW",
+		down: "KeyS",
+		left: "KeyA",
+		right: "KeyD"
+	}
+}
+
+// Calculate data
+data.screen.halfWidth = data.screen.width /2;
+data.screen.halfHeight = data.screen.height /2;
+data.rayCasting.incrementAngle = data.player.fov / data.screen.width;
+data.player.halfov = data.player.fov / 2;
+
+// Creating screen (canvas)
+const screen = document.createElement('canvas');
+screen.width = data.screen.width;
+screen.height = data.screen.height;
+screen.style.border = '2px solid black';
+document.body.appendChild(screen);
+
+// Canvas context
+const screenContext = screen.getContext('2d');
+
+// Estamos usando valores de graus nos atributos, portanto, precisamos
+// criar uma função que converta valores de graus em valores de radianos.
+// Formula: radianos = graus * PI / 180
+
+/**
+ * Cast degree to radian
+ * @param {Number} degree
+ */
+function degreeToRadian(degree) {
+	let pi = Math.PI;
+	return degree * pi / 180;
+}
+
+/**
+ * Draw line into screen
+ * @param {Number} x1
+ * @param {Number} y1
+ * @param {Number} x2
+ * @param {Number} y2
+ * @param {String} cssColor
+ */
+function drawLine(x1, y1, x2, y2, cssColor) {
+	screenContext.strokeStyle = cssColor;
+	screenContext.beginPath();
+	screenContext.moveTo(x1, y1);
+	screenContext.lineTo(x2, y2);
+	screenContext.stroke();
+}
+
+// Start game
+main();
+
+/**
+ * Main loop
+ */
+function main() {
+	setInterval(function() {
+		clearScreen();
+		rayCasting();
+	}, data.render.delay);
+}
+
+/**
+ * RayCasting logic
+ * Para iniciar nossa lógica, precisamos conhecer alguns conceitos com:
+ * player angle, player FOV e Screen width. 
+ * A primeira coisa que precisamos saber é que
+ * cada raio precisa ser lançado em relação ao ângulo do jogador e ao Campo de
+ * Visão (FOV). O FOV do jogador é 60º, mas o foco do jogador está no meio do FOV. 
+ * Por isso temos que iniciar o RayCasting no -30º ângulo do jogador. Por exemplo,
+ * se o ângulo do jogador for 90º, temos que lançar os raios de 60º um ângulo para
+ * 120º outro. Confira a imagem abaixo para ver a representação do FOV do jogador.
+ */
+function rayCasting() {
+	// Dentro da nossa rayCasting()função, podemos obter 
+	// o ângulo real do raio com este código:
+	let rayAngle = data.player.angle - data.player.halfov;
+	/**
+	 * Lançamento dos raios: 
+	 * Temos que iterar todas as fatias da tela, portanto usaremos a largura da tela
+	 * para fazer isso. Para cada iteração, a rayAngle variável precisa ser
+	 * incrementada para iterar todo o FOV do jogador. 
+	 * Usaremos o data.rayCasting.incrementAngle para fazer isso.
+	 */
+	for (let rayCount = 0; rayCount < data.screen.width; rayCount++) {
+/**
+ * Ray data
+ * As primeiras coordenadas do raio são as mesmas das coordenadas do jogador.
+ * Criaremos um objeto com esses valores para ficar mais organizado.
+ */
+	let ray = {
+		x: data.player.x,
+		y: data.player.y
+	}
+
+/**
+ * Ray path incrementers
+ * Para descobrir as próximas coordenadas do raio real, temos que calcular-las
+ * com base no ângulo do raio. Para esta etapa, usaremos as funções Math.sin()
+ * e Math.cos(). Essas funções nos darão os valores de incremento para o raio
+ * a seguir. Nesta etapa usaremos também o atributo precisão, para controlar
+ * o intervalo de cada posição do raio. Os sin e cosn os fornecem valores
+ * flutuantes, mas podemos dividir esses valores com a precisão para
+ * torná-los menores.
+ *
+ * Nota: Quanto maior o valor da precisão, mais verificações serão realizadas
+ * e mais posições o raio terá. Não usaremos um algoritmo DDA que é usado para 
+ * encontrar apenas as interseções na grade, para focar da maneira mais simples.
+ * 
+ * Nota: precisamos usar a degreeToRadians() porque as funções 
+ * trigonométricas nestas funcionam com valores do tipo radianos.
+ */
+	let rayCos = Math.cos(degreeToRadian(rayAngle)) / data.rayCasting.precision;
+	let raySin = Math.sin(degreeToRadian(rayAngle)) / data.rayCasting.precision;
+
+/**
+ * Wall checking
+ * O próximo passo é a verificação da parede. Temos que incrementar as 
+ * coordenadas rayCose raySinpara xe ydo raio até encontrar uma parede no mapa.
+ * Então, nesta etapa precisamos de um loop.
+
+ * Nota: Lembre-se de que as posições da matriz são representadas em regiões 
+ * inteiras. As posições dos raios incrementados cos serão sin valores 
+ * flutuantes e precisamos converter esses valores para o tipo inteiro. 
+ * A função usada para isso é Math.floor()
+ */
+	let wall = 0;
+	while (wall == 0) {
+		ray.x += rayCos;
+		ray.y += raySin;
+		wall = data.map[Math.floor(ray.y)][Math.floor(ray.x)];
+/**
+ * Quando o raio colidir com alguma parede, o loop será interrompido e teremos 
+ * as coordenadas do raio atualizados com a posição da parede. A distância do 
+ * RayCasting é calculada nesta etapa, para saber o tamanho da tira que 
+ * precisaremos desenhar. Se a parede estiver próxima, a distância será menor 
+ * e a linha desenhada será maior. Se a parede estiver longa, a distância 
+ * será maior e a linha desenhada será menor.
+ */
+	}
+
+/**
+ * Pythagoras theorem
+ * Esta fórmula será utilizada com as coordenadas do jogador e as coordenadas 
+ * dos raios da parede.
+ * Teorema de Pitágoras: a² + b² = c²
+ * Fórmula: distance² = (player x - ray x)² + (player y - ray y)²
+ * Código: Math.sqrt(Math.pow(data.player.x - ray.x, 2) + Math.pow(data.player.y - ray.y, 2));
+ */
+	let distance = Math.sqrt(Math.pow(data.player.x - ray.x, 2) + Math.pow(data.player.y - ray.y, 2));
+
+/**
+ * Fish eye fix
+ */
+	distance = distance * Math.cos(degreeToRadian(rayAngle - data.player.angle));
+
+/**
+ * wall height
+ * Com a distância que temos que definir a altura da parede, a altura da parede 
+ * será utilizada para desenhar a faixa na tela. Não podemos usar a distância 
+ * diretamente porque precisamos do valor invertido. Para distância maior, 
+ * faixa menor. Para reverter o valor podemos dividir o valor da distância 
+ * por data.screen.halfHeight. Este valor definirá o mesmo tamanho para 
+ * largura, altura e comprimento.
+
+ * Nota: O wallHeightvalor será utilizado para desenhar a faixa na tela. 
+ * As coordenadas da tela são representadas com um tipo inteiro para o nosso 
+ * caso, portanto, precisamos converter o valor com Math.floor().
+ */
+	let wallHeight = Math.floor(data.screen.halfHeight / distance);
+
+/**
+ * Draw
+ * A altura da parede será usada para desenhar as listras. 
+ * Esta etapa é dividida em três operações.
+
+ * Desenhe o teto (do topo da tela até a meia altura da tela menos a altura da parede)
+ * Desenhe a parede (da meia altura da tela menos a altura da parede até a meia altura da tela mais a altura da parede)
+ * Desenhe o chão (da meia altura da tela mais a altura da parede até a parte inferior da tela)
+ */
+	drawLine(rayCount, 0, rayCount, data.screen.halfHeight - wallHeight, "darkslateblue");
+	drawLine(rayCount, data.screen.halfHeight - wallHeight, rayCount, data.screen.halfHeight + wallHeight, "darkred");
+	drawLine(rayCount, data.screen.halfHeight + wallHeight, rayCount, data.screen.height, "gray");
+
+		// Increment
+		rayAngle += data.rayCasting.incrementAngle;
+	}
+}
+
+/**
+ * Clear screen
+ */
+function clearScreen() {
+	screenContext.clearRect(0, 0, data.screen.width, data.screen.height);
+}
+
+/**
+ * Moviment Event
+ */
+document.addEventListener('keydown', (event) => {
+	let keyCode = event.code;
+
+	if(keyCode == data.key.up) {
+		let playerCos = Math.cos(degreeToRadian(data.player.angle)) * data.player.speed.moviment;
+		let playerSin = Math.sin(degreeToRadian(data.player.angle)) * data.player.speed.moviment;
+		let newX = data.player.x += playerCos;
+		let newY = data.player.y += playerSin;
+
+		// Collision test
+		if(data.map[Math.floor(newY)][Math.floor(newX)] == 0) {
+			data.player.x = newX;
+			data.player.y = newY;
+		}
+
+	} else if(keyCode == data.key.down) {
+		let playerCos = Math.cos(degreeToRadian(data.player.angle)) * data.player.speed.moviment;
+		let playerSin = Math.sin(degreeToRadian(data.player.angle)) * data.player.speed.moviment;
+		let newX = data.player.x -= playerCos;
+		let newY = data.player.y -= playerSin;
+
+		// Collision test
+		if(data.map[Math.floor(newY)][Math.floor(newX)] == 0) {
+			data.player.x = newX;
+			data.player.y = newY;
+		}
+
+	} else if(keyCode == data.key.left) {
+		data.player.angle -= data.player.speed.rotation;
+	} else if(keyCode == data.key.right) {
+		data.player.angle += data.player.speed.rotation;
+	}
+});
